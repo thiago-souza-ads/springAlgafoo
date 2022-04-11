39922be25f713cbc841fe054fdb665968dc5565e
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-04-11 19:38:17 -0300

    ATUALIZAÇÕES

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,8 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into cozinha (nome) values ('Australiana');
+insert into cozinha (nome) values ('Polonesa');
+insert into cozinha (nome) values ('Brasileira');
+insert into cozinha (nome) values ('Mexicana');
+insert into cozinha (nome) values ('Portuguesa');
+insert into cozinha (nome) values ('Americana');
