423b4fbc9ba0fc6e039a328e398a7a650c16ea21
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-05-22 13:35:16 -0300

    Ajuste de exibicao

--- src/main/java/com/algaworks/algafood/jpa/ConsultaRestauranteMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaRestauranteMain.java
@@ -1,16 +1,12 @@
 package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 public class ConsultaRestauranteMain {
