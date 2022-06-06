c16468a6bf4e0d928655fde64d0ee836213abc36
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-06-06 20:26:31 -0300

    Ajuste de representação em XML

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,9 +2,9 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +17,13 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listarJSON() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cozinha> listarXML() {
         return cozinhaRepository.listar();
     }
 
