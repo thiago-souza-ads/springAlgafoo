b51abb8858df849ef56c09ca5ad7d6ae7ce610a7
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-06-14 20:15:51 -0300

    Ajuste endentação

--- src/main/java/com/algaworks/algafood/api/model/ModeloXmlWrapper.java
+++ src/main/java/com/algaworks/algafood/api/model/ModeloXmlWrapper.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood.api.model;
 //Modelo de Representação de um recurso
 
 import com.algaworks.algafood.domain.model.Modelo;
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NonNull;
