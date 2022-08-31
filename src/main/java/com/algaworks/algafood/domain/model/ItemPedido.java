7d46e0a74183c1b92eae07487d7aa92c65ae7541
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-08-30 23:00:34 -0300

    Ajuste Imports

--- src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.repository.cdi.Eager;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
