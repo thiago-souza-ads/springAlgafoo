309a576efc458be449d2838fb6f6b4ab52d8df17
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-10-25 19:42:20 -0300

    Aplicando groups Classe cidade, agrupando validações

--- src/main/java/com/algaworks/algafood/domain/dto/CidadeDto.java
+++ src/main/java/com/algaworks/algafood/domain/dto/CidadeDto.java
@@ -1,20 +1,27 @@
 package com.algaworks.algafood.domain.dto;
 
+import com.algaworks.algafood.domain.validators.Groups;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class CidadeDto implements Serializable {
 
+    @NotNull(groups = Groups.CidadeId.class)
     @EqualsAndHashCode.Include
     private final Long id;
 
+    @NotBlank
     private final String nome;
 
+    @NotNull
     private final Boolean isCapital;
 
+    @NotNull
     private final EstadoDto estado;
 }
