3c0702024488e93cca57ce0c5d46e1a4ffd247fe
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-10-25 19:38:24 -0300

    Criação de Groups todas as classes, agrupando validações

--- src/main/java/com/algaworks/algafood/domain/validators/Groups.java
+++ src/main/java/com/algaworks/algafood/domain/validators/Groups.java
@@ -2,6 +2,17 @@ package com.algaworks.algafood.domain.validators;
 
 public interface Groups {
     public interface CozinhaId {}
-
-
+    public interface CidadeId {}
+    public interface EnderecoId {}
+    public interface EstadoId {}
+    public interface FormaDePafamentoId {}
+    public interface GrupoId {}
+    public interface ItemPedidoId {}
+    public interface PaisId {}
+    public interface PedidoId {}
+    public interface PermissaoId {}
+    public interface ProdutoId {}
+    public interface RegiaoId {}
+    public interface RestauranteId {}
+    public interface UsuarioId {}
 }
