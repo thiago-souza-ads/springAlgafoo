2a87a5ab055a5dd1cc58119bd970518f7d42a687
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-10-25 19:59:49 -0300

    Aplicando groups Classe cidade, agrupando validações

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -15,12 +17,15 @@ public class Cidade {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @NotNull
     @Column(name = "is_Capital")
     private Boolean isCapital;
 
+    @NotNull
     @ManyToOne // <--- Muitas Cidades tem um Estado = Relacionamento muitos para um
     @JoinColumn(name = "estado_id", nullable = false)
     private Estado estado;
