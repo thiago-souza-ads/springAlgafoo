69775e05fd08fe5da5d4d092f079ba262d18ea4d
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-11-13 13:32:43 -0300

    Preparando o projeto para testes de integração

--- src/test/java/com/algaworks/algafood/AlgafoodApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApplicationTests.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertFalse;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlgafoodApplicationTests {
+public class AlgafoodApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        assertFalse(true);
     }
 
 }
