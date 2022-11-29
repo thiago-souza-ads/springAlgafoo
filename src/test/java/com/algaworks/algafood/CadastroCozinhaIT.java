81e171f6b4b8a288a5c9bf218dae6b5d58dad29c
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2022-11-28 21:43:28 -0300

    Implementando Testes de API com REST Assured e validando o código de status HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -4,22 +4,34 @@ import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import io.restassured.http.ContentType;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.ConstraintViolationException;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
 
     @Autowired
     private CadastroCozinhaService cadastroCozinhaService;
+
+    @LocalServerPort
+    private int port;
+
+    /**
+     * Testes de Integração
+     */
 
     @Test
     public void deveCadastrarCozinhaComSucesso_QuandoCozinhaCorreta() {
@@ -51,8 +63,24 @@ public class CadastroCozinhaIT {
     }
 
     @Test(expected = EntidadeNaoEncontradaException.class)
-    public void deveFalharAoExcluirCozinha_QuandoCozinhaInexistente(){
+    public void deveFalharAoExcluirCozinha_QuandoCozinhaInexistente() {
         cadastroCozinhaService.excluir(123456L);
+    }
+
+    /**
+     * Testes de API
+     */
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .statusCode(HttpStatus.OK.value());
     }
 
 }
