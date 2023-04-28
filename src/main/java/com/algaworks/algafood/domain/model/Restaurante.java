e3c3115054cfc8efc0a6cf921dff2779950276d6
Thiago Rodrigues de Souza <91929392+thiago-souza-ads@users.noreply.github.com>
2023-04-28 14:39:24 -0300

    Configurando exibição de data e hora para adequar timezone UTC

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -17,6 +17,7 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,11 +53,11 @@ public class Restaurante {
 
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataAtualizacao;
+    private OffsetDateTime dataAtualizacao;
 
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
